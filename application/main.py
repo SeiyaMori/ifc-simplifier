5c6d4022e7457231348b13ef6b515f7ce00f7e8d
seiyamori <seiyamori95@gmail.com>
2024-03-22 21:54:04 +0000

    WIP

--- application/main.py
+++ application/main.py
@@ -1,9 +1,50 @@
 # main.py
 
 import sys
-from PyQt6.QtCore import QSize, Qt, QAbstractTableModel, QModelIndex
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QStyledItemDelegate
+from PyQt6.QtCore import QSize, Qt, QAbstractTableModel, QModelIndex, QEvent
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStyleOptionViewItem, QTableView, QStyledItemDelegate, QItemDelegate, QCheckBox
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
+
+
+class CheckBoxDelegate(QItemDelegate):
+    """
+    A delegate that places a fully functioning QCheckBox cell of the column to which it's applied.
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def createEditor(self, parent, option, index):
+        """
+        Important, otherwise an editor is created if the user clicks in this cell.
+        """
+        return None
+
+    def paint(self, painter, option, index):
+        """
+        Paint a checkbox without the label.
+        """
+        self.drawCheck(painter, option, option.rect, Qt.CheckState.Unchecked if int(index.data()) == 0 else Qt.CheckState.Checked)
+
+    def editorEvent(self, event, model, option, index):
+        '''
+        Change the data in the model and the state of the checkbox
+        if the user presses the left mousebutton and this cell is editable. Otherwise do nothing.
+        '''
+
+        if event.type() == QEvent.Type.MouseButtonRelease and event.button() == Qt.MouseButton.LeftButton:
+            # Change the checkbox-state
+            self.setModelData(None, model, index)
+            return True
+
+        return False
+
+
+    def setModelData (self, editor, model, index):
+        '''
+        The user wanted to change the old state in the opposite.
+        '''
+        model.setData(index, 1 if int(index.data()) == 0 else 0, Qt.ItemDataRole.EditRole)
+
 
 
 class ReadOnlyDelegate(QStyledItemDelegate):
@@ -23,11 +64,13 @@ class MainWindow(QMainWindow):
         self.setMaximumSize(QSize(1200,900))
 
         self._table_view = QTableView(self)
-        self._table_view.setItemDelegate(ReadOnlyDelegate())
+        #self._table_view.setItemDelegate(ReadOnlyDelegate())
         #self._table_view.setSelectionMode(QTableView.SelectionMode.NoSelection)
         #self._table_view.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
+        #self._table_view.setEditTriggers(QTableView.EditTrigger.EditKeyPressed)
         #self._table_view.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self._table_view.setSelectionBehavior(QTableView.SelectionBehavior.SelectRows)
+        self._table_view.setItemDelegateForColumn(4, CheckBoxDelegate())
         self.setCentralWidget(self._table_view)
 
         self.db = QSqlDatabase.addDatabase("QSQLITE")  
@@ -47,6 +90,7 @@ class MainWindow(QMainWindow):
 
             # Edit strategy
             self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnFieldChange)
+
 
             if self.model.select():
                 self._table_view.setModel(self.model)
